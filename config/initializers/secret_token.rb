f1a8fb83ec437911140b21ffdf2729c9be2850fd
Vyenna Song <vyenna.song@spreemo.com>
2014-08-06 15:18:52 -0400

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '0d631630d9520719dd0f359bb1ce89bcf1b5301b87270f1f9222f295a18353a25ecf63a145185e45216a2fac1ca8bdd80e81401f6a2e51fcd1432bf22c860c89'
